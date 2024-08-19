1757fd051aa382a638d20f2bf95b8b9ae4139b5f
DOCYOONY <162411345+DOCYOONY@users.noreply.github.com>
2024-08-19 18:05:47 +0900

    Delete src directory

--- src/main/java/hello/imagine/Meeting/Repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package hello.imagine.Group.Repository;
-
-import hello.imagine.Group.Entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
--- src/main/java/hello/imagine/login/exception/CustomDuplicateException.java
+++
@@ -1,7 +0,0 @@
-package hello.imagine.login.exception;
-
-public class CustomDuplicateException extends Exception{
-    public CustomDuplicateException(String message) {
-        super(message);
-    }
-}
