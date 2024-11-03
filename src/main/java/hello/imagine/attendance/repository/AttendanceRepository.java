4d924b68f0ef11da9034102da003c0f03cf2bbb1
DOCYOONY <docyoony@gmail.com>
2024-11-04 00:58:24 +0900

    11.04 chul

--- src/main/java/hello/imagine/attendance/repository/AttendanceRepository.java
+++ src/main/java/hello/imagine/attendance/repository/AttendanceRepository.java
@@ -11,6 +11,5 @@ import java.util.List;
 @Repository
 public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
     boolean existsByMemberAndDate(Member member, LocalDate date);
-
     List<Attendance> findByMemberAndDateBetween(Member member, LocalDate startDate, LocalDate endDate);
 }
